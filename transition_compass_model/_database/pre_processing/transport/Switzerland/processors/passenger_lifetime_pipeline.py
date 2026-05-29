9ed17996acfb6762bab369f68e0c3dbaaad9d100
edoardochiarotti <62812416+edoardochiarotti@users.noreply.github.com>
2026-05-29 18:35:29 +0200

    Update passenger_lifetime_pipeline.py
    the src.api.routes was an old call, it has been updated, and following the update the lifetime of some vehicles techs was added (before it was nan)

--- transition_compass_model/_database/pre_processing/transport/Switzerland/processors/passenger_lifetime_pipeline.py
+++ transition_compass_model/_database/pre_processing/transport/Switzerland/processors/passenger_lifetime_pipeline.py
@@ -1,12 +1,10 @@
 ########################
 ####    LIFETIME    ####
 ########################
-from src.api.routes import country_list
-
 from transition_compass_model.model.common.data_matrix_class import DataMatrix
 
 
-def run(mode_cat, tech_cat, years_ots, years_fts, country_years):
+def run(mode_cat, tech_cat, years_ots, years_fts, country_list):
     # region Reflection on the renewal-rate and lifetime
     # When the stock is constant, the renewal-rate is 1/lifetime. This gives a logical sense to the renewal-rate.
     # For the ots, the renewal-rate is simply computed as waste/stock, and its value is often far from what we believe
@@ -57,9 +55,12 @@ def run(mode_cat, tech_cat, years_ots, years_fts, country_years):
     dm_lifetime[:, :, "tra_passenger_lifetime", "2W", :] = 8
     # We assume rail lifetime is 30 years, metrotram lifetime is 20 years and bus lifetime is 10 years
     dm_lifetime[:, :, "tra_passenger_lifetime", "rail", "CEV"] = 30
+    dm_lifetime[:, :, "tra_passenger_lifetime", "rail", "ICE-diesel"] = 30
+    dm_lifetime[:, :, "tra_passenger_lifetime", "rail", "mt"] = 30
     dm_lifetime[:, :, "tra_passenger_lifetime", "metrotram", "mt"] = 20
     dm_lifetime[:, :, "tra_passenger_lifetime", "bus", "CEV"] = 10
     dm_lifetime[:, :, "tra_passenger_lifetime", "bus", "ICE-diesel"] = 10
+    dm_lifetime[:, :, "tra_passenger_lifetime", "bus", "mt"] = 10
 
     dm_lifetime.fill_nans("Years")
 
